946f1b1456e68a5d4497de577972bacd794b0842
autekroy <autek.roy@gmail.com>
2015-10-15 22:01:19 -0700

    useful command

---
+++ Useful_command.md
@@ -0,0 +1,10 @@
+Useful command
+
+### update
+bundle install
+rake db:migrate
+rails server
+
+### drop table
+rails console
+ActiveRecord::Migration.drop_table(:users)
--- db/schema.rb
+++ db/schema.rb
@@ -28,13 +28,4 @@ ActiveRecord::Schema.define(version: 20151016045111) do
     t.datetime "updated_at",  null: false
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string   "email"
-    t.string   "password"
-    t.string   "username"
-    t.string   "phone"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
 end
