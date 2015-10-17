b48756b79035cd1360832d9d9220c243179a0133
autekroy <autek.roy@gmail.com>
2015-10-16 17:38:39 -0700

    basic link to functions

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,24 @@
 
 ActiveRecord::Schema.define(version: 20151016235439) do
 
+  create_table "models", force: :cascade do |t|
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "models", ["email"], name: "index_models_on_email", unique: true
+  add_index "models", ["reset_password_token"], name: "index_models_on_reset_password_token", unique: true
+
   create_table "pickups", force: :cascade do |t|
     t.string   "uid"
     t.string   "name"
