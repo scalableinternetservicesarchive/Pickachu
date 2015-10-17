90708ffae7f2d32148105ffc57734502f6d3ef25
autekroy <autek.roy@gmail.com>
2015-10-17 15:45:42 -0700

    change config to use mysql

--- db/schema.rb
+++ db/schema.rb
@@ -14,44 +14,44 @@
 ActiveRecord::Schema.define(version: 20151016235439) do
 
   create_table "models", force: :cascade do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
-    t.string   "reset_password_token"
+    t.string   "email",                  limit: 255, default: "", null: false
+    t.string   "encrypted_password",     limit: 255, default: "", null: false
+    t.string   "reset_password_token",   limit: 255
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
+    t.integer  "sign_in_count",          limit: 4,   default: 0,  null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip"
-    t.string   "last_sign_in_ip"
+    t.string   "current_sign_in_ip",     limit: 255
+    t.string   "last_sign_in_ip",        limit: 255
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
-  add_index "models", ["email"], name: "index_models_on_email", unique: true
-  add_index "models", ["reset_password_token"], name: "index_models_on_reset_password_token", unique: true
+  add_index "models", ["email"], name: "index_models_on_email", unique: true, using: :btree
+  add_index "models", ["reset_password_token"], name: "index_models_on_reset_password_token", unique: true, using: :btree
 
   create_table "pickups", force: :cascade do |t|
-    t.string   "uid"
-    t.string   "name"
-    t.decimal  "long"
-    t.decimal  "lat"
-    t.integer  "type"
-    t.string   "image_url"
-    t.decimal  "price"
+    t.string   "uid",         limit: 255
+    t.string   "name",        limit: 255
+    t.decimal  "long",                      precision: 10
+    t.decimal  "lat",                       precision: 10
+    t.integer  "type",        limit: 4
+    t.string   "image_url",   limit: 255
+    t.decimal  "price",                     precision: 10
     t.date     "start_time"
     t.date     "end_time"
-    t.text     "description"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.text     "description", limit: 65535
+    t.datetime "created_at",                               null: false
+    t.datetime "updated_at",                               null: false
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "name"
-    t.string   "email"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
-    t.string   "password_digest"
+    t.string   "name",            limit: 255
+    t.string   "email",           limit: 255
+    t.datetime "created_at",                  null: false
+    t.datetime "updated_at",                  null: false
+    t.string   "password_digest", limit: 255
   end
 
 end
