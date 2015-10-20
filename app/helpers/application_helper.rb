af60a3523d9f22043f99927b43d59d45eb87f3e3
autekroy <autek.roy@gmail.com>
2015-10-20 01:02:00 -0700

    google map may need to start again

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
 module ApplicationHelper
+	# def google_maps_api_key
+	# 	"AIzaSyBqGHXqWZ2hO2atUBSMRZluvbz0UHCMCv4"
+	# end
+	# def google_api_url
+	# 	"http://maps.googleapis.com/maps/api/js"
+	# end
+	# def google_api_access
+	# 	"#{google_api_url}?key=#{google_maps_api_key}&amp;libraries=geometry&amp;sensor=false"
+	# end
+	# def google_maps_api
+	# 	content_tag(:script,:type =&gt; "text/javascript",:src =&gt; google_api_access) do
+	# 	end
+	# end
 end
