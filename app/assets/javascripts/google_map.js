a5dca53624b5fd5fb48b269375c850fe60d6dc57
autekroy <autek.roy@gmail.com>
2015-10-20 20:29:11 -0700

    google map on the bottom

--- app/assets/javascripts/google_map.js
+++ app/assets/javascripts/google_map.js
@@ -5,6 +5,16 @@ var map;
 //     zoom: 8
 //   });
 // }
+// function initialize() {
+//   var mapOptions = {
+//       center: new google.maps.LatLng(34.067, -118.445),
+//       zoom: 15,
+//       mapTypeId: google.maps.MapTypeId.ROADMAP
+//   };
+//   map = new google.maps.Map(document.getElementById("map_canvas"),
+//   mapOptions);
+// }
+
 
 function initialize() {
   // var mapOptions = {
@@ -14,9 +24,9 @@ function initialize() {
   // var map = new google.maps.Map(document.getElementById('map-canvas'),
       // mapOptions);
 
-  map = new google.maps.Map(document.getElementById('map-canvas'), {
-    center: {lat: -34.397, lng: 150.644},
-    zoom: 8
+  map = new google.maps.Map(document.getElementById('map_canvas'), {
+    center: {lat: 34.067, lng: -118.445},
+    zoom: 14
   });
 
   // for (var i = 0; i < notes.length; i++) {
