28f8e8696509e1a0c7269f7e77aba176f29c321a
autekroy <autek.roy@gmail.com>
2015-10-22 18:49:07 -0700

    map for detecting current and move marker freely and marker content

--- app/assets/javascripts/google_map.js
+++ app/assets/javascripts/google_map.js
@@ -1,29 +1,8 @@
 var map;
-// function initMap() {
-//   map = new google.maps.Map(document.getElementById('map-canvas'), {
-//     center: {lat: -34.397, lng: 150.644},
-//     zoom: 8
-//   });
-// }
-// function initialize() {
-//   var mapOptions = {
-//       center: new google.maps.LatLng(34.067, -118.445),
-//       zoom: 15,
-//       mapTypeId: google.maps.MapTypeId.ROADMAP
-//   };
-//   map = new google.maps.Map(document.getElementById("map_canvas"),
-//   mapOptions);
-// }
+var maker;
 
 
 function initialize() {
-  // var mapOptions = {
-  //     center: new google.maps.LatLng(notes[0].latitude, notes[0].longitude),
-  //     zoom: 8
-  // };
-  // var map = new google.maps.Map(document.getElementById('map-canvas'),
-      // mapOptions);
-
   map = new google.maps.Map(document.getElementById('map_canvas'), {
     center: {lat: 34.067, lng: -118.445},
     zoom: 14
@@ -37,4 +16,58 @@ function initialize() {
   //     body: notes[i].body
   //   });
   // }
+  marker = new google.maps.Marker({
+          map: map,
+          draggable: true,
+          // title: 'Old book for free',
+          animation: google.maps.Animation.DROP
+          // position: pos
+        });
+  var pos;
+  // Try HTML5 geolocation.
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function(position) {
+      pos = {
+        lat: position.coords.latitude,
+        lng: position.coords.longitude  
+      };
+      marker.setPosition(pos);
+      map.setCenter(pos);
+    }, function() {
+      handleLocationError(true, infoWindow, map.getCenter());
+    });
+  } else {
+    // Browser doesn't support Geolocation
+    handleLocationError(false, infoWindow, map.getCenter());
+  }
+  
+  var contentString = '<div id="content">'+
+      '<div id="siteNotice">'+
+      '</div>'+
+      '<h1 id="firstHeading" class="firstHeading">Old book for FREE!</h1>'+
+      '<div id="bodyContent">'+
+      'I want to give my old books to someone who needs them!';
+  var infowindow = new google.maps.InfoWindow({
+    content: contentString
+  });      
+
+  // marker.addListener('click', toggleBounce);
+  marker.addListener('click', function() {
+    infowindow.open(map, marker);
+  });
+}
+
+// function toggleBounce() {
+//   if (marker.getAnimation() !== null) {
+//     marker.setAnimation(null);
+//   } else {
+//     marker.setAnimation(google.maps.Animation.BOUNCE);
+//   }
+// }
+
+function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+  infoWindow.setPosition(pos);
+  infoWindow.setContent(browserHasGeolocation ?
+                        'Error: The Geolocation service failed.' :
+                        'Error: Your browser doesn\'t support geolocation.');
 }
