406bcf45f20014b22f1c0056a6096a4854b9b609
guanbeiqi@gmail.com <guanbeiqi@jiuzhilan.net>
2015-11-12 16:51:34 -0800

    some minor changes

--- config/routes.rb
+++ config/routes.rb
@@ -13,9 +13,9 @@ Rails.application.routes.draw do
   end
 
 
-  get 'login' => "home#login"
+  # get 'login' => "home#login"
 
-  get 'about' => "home#about"
+  # get 'about' => "home#about"
 
   get 'pickachu' => "home#pickachu"
 
@@ -23,9 +23,9 @@ Rails.application.routes.draw do
 
   get 'pickups' =>"pickups#index"
 
-  get 'Login' =>'logini#login'
+  # get 'Login' =>'logini#login'
 
-  get 'signup' => 'users#new'
+  # get 'signup' => 'users#new'
 
   #Route for comment action
   get 'comment' => 'users#comment'
