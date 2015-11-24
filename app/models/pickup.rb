d2fd971e25ba1efff6cb5fed9c08140c6682b81e
autekroy <autek.roy@gmail.com>
2015-11-23 23:37:02 -0800

    pickup load test

--- app/models/pickup.rb
+++ app/models/pickup.rb
@@ -30,23 +30,4 @@ class Pickup < ActiveRecord::Base
             lat + miles.to_f/69.712)
   end
 
-
-
-# 3959 是以 mile 為單位的常數，若要以 km 計，請替換成 6371
-# (37, -122) 是你傳入的位置
-# (lat, lng) 是 database 裡面，經緯度的 column name
-# markers 是 database 的 table name
-# SELECT id, ( 3959 * acos( cos( radians(37) ) * cos( radians( lat ) )
-# * cos( radians( lng ) - radians(-122) ) + sin( radians(37) ) * sin(radians(lat)) ) ) AS distance
-# FROM markers
-
-# HAVING distance < 25
-# ORDER BY distance
-# LIMIT 0 , 20;
-
-# HAVING distance < 25
-# ORDER BY distance
-# LIMIT 0 , 20;
-# where(:title, query) -> This would return an exact match of the query
-
 end
