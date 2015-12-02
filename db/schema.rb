70138ba2188d26ef54784b0be0b5a2283601b724
demundo <aristurtledemundo@gmail.com>
2015-12-01 21:15:23 -0800

    support seed for multi-passenger

--- db/schema.rb
+++ db/schema.rb
@@ -13,24 +13,6 @@
 
 ActiveRecord::Schema.define(version: 20151130234129) do
 
-  create_table "models", force: :cascade do |t|
-    t.string   "email",                  limit: 255, default: "", null: false
-    t.string   "encrypted_password",     limit: 255, default: "", null: false
-    t.string   "reset_password_token",   limit: 255
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          limit: 4,   default: 0,  null: false
-    t.datetime "current_sign_in_at"
-    t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip",     limit: 255
-    t.string   "last_sign_in_ip",        limit: 255
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  add_index "models", ["email"], name: "index_models_on_email", unique: true, using: :btree
-  add_index "models", ["reset_password_token"], name: "index_models_on_reset_password_token", unique: true, using: :btree
-
   create_table "nearbies", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
